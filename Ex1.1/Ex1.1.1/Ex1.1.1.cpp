c8199638c3e4f49d049bce04fe277277a67b1c52
Vlad Dzhuromsky <95527000+Inkwizzz@users.noreply.github.com>
2023-02-16 00:14:33 +0300

    1st Ex

---
+++ Ex1.1/Ex1.1.1/Ex1.1.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "Utils.h"
+using namespace std;
+
+int main()
+{
+    int x = 3, y = 5;
+    cout << Plus(x, y) << endl;
+}
+/*
+int x, y;
+    cout << "x = "; cin >> x; //ввод х
+    cout << "y = "; cin >> y; //ввод y
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " / " << y << " = " << x / y << endl;
+    cout << x << " % " << y << " = " << x % y << endl;
+    cout << x << " | " << y << " = " << (x | y) << endl;
+    cout << x << " & " << y << " = " << (x & y) << endl;
+    cout << x << " ^ " << y << " = " << (x ^ y) << endl;
+    cout << x << " << " << y << " = " << x << y << endl;
+    cout << x << " >> " << y << " = " << (x >> y) << endl;
+    */
