ad7ccf62a9edf2f6bb4b04fe746c72e65b2bf5c9
Vlad Dzhuromsky <95527000+Inkwizzz@users.noreply.github.com>
2023-05-31 22:45:23 +0300

    Написал BoxPlot & Timer

--- Ex6a/Ex6a.cpp
+++ Ex6a/Ex6a.cpp
@@ -1,7 +1,9 @@
 int replace(char ch_old, char ch_new) 
 {
     int count = 0;
-    {
+    
+ for (char& ch : data_) 
+	{
         if (ch == ch_old) 
         {
             ch = ch_new;
---
+++ Timer_BP/Timer_BP/Timer_BP.cpp
@@ -0,0 +1,68 @@
+﻿#include <chrono>
+#include <iostream>
+
+using namespace std;
+
+long fibonacci(unsigned n) {
+    return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+template <typename T>
+class Timer {
+public:
+    static void reset();
+    static T elapsed_nanoseconds();
+    static T elapsed_microseconds();
+    static T elapsed_milliseconds();
+    static T elapsed_seconds();
+    static T elapsed_minutes();
+private:
+    typedef std::chrono::high_resolution_clock clock_;
+    static std::chrono::time_point<std::chrono::high_resolution_clock> beg_;
+};
+
+template <typename T>
+void Timer<T>::reset() {
+    beg_ = clock_::now();
+}
+
+template <typename T>
+T Timer<T>::elapsed_nanoseconds() {
+    return std::chrono::duration_cast<std::chrono::duration<T, std::nano>>(clock_::now() - beg_).count();
+}
+
+template <typename T>
+T Timer<T>::elapsed_microseconds() {
+    return std::chrono::duration_cast<std::chrono::duration<T, std::micro>>(clock_::now() - beg_).count();
+}
+
+template <typename T>
+T Timer<T>::elapsed_milliseconds() {
+    return std::chrono::duration_cast<std::chrono::duration<T, std::milli>>(clock_::now() - beg_).count();
+}
+
+template <typename T>
+T Timer<T>::elapsed_seconds() {
+    return std::chrono::duration_cast<std::chrono::duration<T, std::ratio<1>>>(clock_::now() - beg_).count();
+}
+
+template <typename T>
+T Timer<T>::elapsed_minutes() {
+    return std::chrono::duration_cast<std::chrono::duration<T, std::ratio<60>>>(clock_::now() - beg_).count();
+}
+
+template<typename T>
+std::chrono::time_point<std::chrono::high_resolution_clock> Timer<T>::beg_ = std::chrono::high_resolution_clock::now();
+
+int main() {
+    Timer<double>::reset();
+    std::cout << "f(42) = " << fibonacci(42) << '\n';
+    std::cout << "Elapsed time in minutes: " << Timer<double>::elapsed_minutes() << std::endl;
+    std::cout << "Elapsed time in seconds: " << Timer<double>::elapsed_seconds() << std::endl;
+    std::cout << "Elapsed time in milliseconds: " << Timer<double>::elapsed_milliseconds() << std::endl;
+    std::cout << "Elapsed time in microseconds: " << Timer<double>::elapsed_microseconds() << std::endl;
+    std::cout << "Elapsed time in nanoseconds: " << Timer<double>::elapsed_nanoseconds() << std::endl;
+    
+    return 0;
+}
+
